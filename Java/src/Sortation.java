213cf4fa6456e39a99e063a25b4df61d8e39ee09
jesse winden <winden.jesse@gmail.com>
2020-03-17 00:53:32 -0400

    Move Method, Rename

--- Java/src/Sortation.java
+++ Java/src/Sortation.java
@@ -2,65 +2,71 @@ import java.io.*;
 import java.util.Random;
 
 public class Sortation {
+
     public static int Comp = 0;
     public static int Swap = 0;
-    public static  void main(String args[]) throws IOException  {
-        BufferedReader obj = new BufferedReader(new InputStreamReader(System.in));
+    public static BufferedReader obj = new BufferedReader(new InputStreamReader(System.in));
 
+    public static  void main(String args[])throws IOException{
+        ChooseSortMethod();
+    }
 
-
+    public static void ChooseSortMethod() throws IOException{
 
         String answer = "";
+        System.out.println("Hello Friends: Choose One: Selection (s), Bubble(b), Insertion(i), or Quit(q): ");
+        answer = obj.readLine();
 
-        do
+        int[] numbers = createArray();
+
+        //region Bubble
+        if (answer.equals("b"))
         {
-            System.out.println("Hello Friends: Choose One: Selection (s), Bubble(b), Insertion(i), or Quit(q): ");
-            answer = obj.readLine();
-            System.out.println("How many numbers?: ");
-            String num = obj.readLine();
-            int choices = Integer.parseInt(num);
+            Bubble bubble = new Bubble();
+            bubble.BubbleSort(numbers);
+        }//end bubble
 
-            int[] numbers = new int[choices];
-            Random rand = new Random();
+        //region Selection
+        if (answer.equals("s"))
+        {
+            Selection selection = new Selection();
+            selection.SelectionSort(numbers);
+        }//end Selection
 
-            for (int y = 0; y < choices; y++)
-            {
-                int randomnum = rand.nextInt(100);
-                numbers[y] = randomnum;
-            }
+        //region Insertions
+        if (answer.equals("i"))
+        {
+            Insertion insertion = new Insertion();
+            insertion.InsertionSort(numbers);
+        }//end Insertions
 
-            //region Bubble
-            if (answer.equals("b"))
-            {
-                Bubble bubble = new Bubble();
-                bubble.BubbleSort(numbers);
-            }//end bubble
+        displayResult(numbers);
+        ChooseSortMethod();
+    }
 
-            //region Selection
-            if (answer.equals("s"))
-            {
-                Selection selection = new Selection();
-                selection.SelectionSort(numbers);
-            }//end Selection
+    public static void displayResult(int[] numbers) throws IOException{
+        for (int x = 0; x < numbers.length; x++)
+        {
+            System.out.print(" " + numbers[x]);
+        }
 
-            //region Insertions
-            if (answer.equals("i"))
-            {
-                Insertion insertion = new Insertion();
-                insertion.InsertionSort(numbers);
-            }//end Insertions
+        System.out.println("\nCompared: " + Comp);
+        System.out.println("Swapped: " +  Swap);
+        System.out.println("<Press Enter>");
+        obj.readLine();
+    }
+    public static int[] createArray() throws IOException{
+        System.out.println("How many numbers?: ");
+        String num = obj.readLine();
+        int[] numbers = new int[Integer.parseInt(num)];
+        Random rand = new Random();
 
-            for (int x = 0; x < numbers.length; x++)
-            {
-                System.out.print(" " + numbers[x]);
-            }
-
-            System.out.println("\nCompared: " + Comp);
-            System.out.println("Swapped: " +  Swap);
-            System.out.println("<Press Enter>");
-            obj.readLine();
-
-        } while (answer != "q");
+        for (int y = 0; y < numbers.length; y++)
+        {
+            int randomnum = rand.nextInt(100);
+            numbers[y] = randomnum;
+        }
+        return numbers;
     }
 
 }
